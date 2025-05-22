e0329a96f521dbe2a7a3f2dd89c8d97bac3de01f
Imam Bahri Alwi <115518832+alwi2022@users.noreply.github.com>
2025-05-22 22:39:09 +0700

    chore: add custom domain finance.alwi.tech and link to Heroku app

--- backend/src/config/db-config.ts
+++ backend/src/config/db-config.ts
@@ -1,12 +1,14 @@
-import mongoose from "mongoose"
-
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI!, {})
-        console.log("MongoDB connected")
-    } catch (error) {
-        console.log("Error connecting to MongoDB", error)
-        process.exit(1)
-    }
-}
+  try {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) throw new Error("MONGODB_URI is not defined");
+
+    await mongoose.connect(uri);
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB", error);
+    process.exit(1);
+  }
+};
